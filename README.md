9ef23616aee0ab36b3dc69243e19b6a7b5f21d66
nik <nik@heartex.net>
2019-05-19 02:53:50 +0300

    add requires

--- README.md
+++ README.md
@@ -12,5 +12,7 @@ pip3 install -r requirements.txt
 python3 stargazers.py --repos https://github.com/josephmisiti/awesome-machine-learning https://github.com/tensorflow/models \
 --login my_github_login \
 --password my_github_password \
---output-file stargazers.tsv
+--output-file stargazers.tsv \
+--email-required \
+--company-required
 ```
--- stargazers.py
+++ stargazers.py
@@ -6,25 +6,30 @@ from github_stargazers.github import GitHub as GitHubStargazer
 from github import Github
 
 
-def main(repos, login, password, output_file):
+def main(args):
     output = []
-    g = Github(login, password)
+    g = Github(args.login, args.password)
     prefix = 'https://github.com/'
-    for repo in repos:
+    for repo in args.repos:
         reponame = re.sub(prefix, '', repo)
         print(f'Start processing {reponame}...')
         gs = GitHubStargazer(reponame)
         stargazers = gs.get_all_stargazers()
         for stargazer in stargazers:
             user = g.get_user(stargazer)
-            if user.name and user.email and user.company:
-                output.append({
-                    'name': user.name,
-                    'email': user.email,
-                    'company': user.company,
-                    'location': user.location
-                })
-    pd.DataFrame.from_records(output).to_csv(output_file, sep='\t', index=False)
+            if args.email_required and not user.email:
+                continue
+            if args.company_required and not user.company:
+                continue
+            if args.location_required and not user.location:
+                continue
+            output.append({
+                'name': user.name,
+                'email': user.email,
+                'company': user.company,
+                'location': user.location
+            })
+    pd.DataFrame.from_records(output).to_csv(args.output_file, sep='\t', index=False)
 
 
 if __name__ == "__main__":
@@ -37,7 +42,10 @@ if __name__ == "__main__":
                         help='Your GitHub password')
     parser.add_argument('--output-file', dest='output_file', default='output.tsv',
                         help='Output file in TSV format')
+    parser.add_argument('--email-required', dest='email_required', action='store_true')
+    parser.add_argument('--company-required', dest='company_required', action='store_true')
+    parser.add_argument('--location-required', dest='location_required', action='store_true')
 
     args = parser.parse_args()
 
-    main(args.repos, args.login, args.password, args.output_file)
+    main(args)
